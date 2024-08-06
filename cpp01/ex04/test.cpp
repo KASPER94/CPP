342fab5ea16d11ce3ae00c3a1bb3a92d44806c64
Simon Kaperski <skapersk@paul-f3Br4s5.clusters.42paris.fr>
2024-08-06 12:25:18 +0200

    tester to finish

--- cpp01/ex04/test.cpp
+++ cpp01/ex04/test.cpp
@@ -6,11 +6,11 @@
 /*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 11:00:32 by skapersk          #+#    #+#             */
-/*   Updated: 2024/08/05 15:56:50 by skapersk         ###   ########.fr       */
+/*   Updated: 2024/08/06 12:22:10 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "EditFile.hpp"
+#include "./includes/EditFile.hpp"
 #include <stdio.h>
 #include <iostream>
 
@@ -24,7 +24,7 @@ void	exec(std::string str)
 
 int	main(void)
 {
-	exec("../EditFile ../Makefile # A");
-	getchar();
+	exec("./EditFile ./Makefile '#' A");
+	// getchar();
 	return (0);
 }
