3412f03ec44123923570dfe7ff8805177192dc48
skapersk <simon.kaperski@gmail.com>
2024-08-11 18:16:31 +0200

    starting cpp04

---
+++ cpp04/ex00/sources/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 15:18:38 by skapersk          #+#    #+#             */
+/*   Updated: 2024/08/11 15:22:04 by skapersk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+
+	return 0;
+}
