aab324180530dc0ac0e6947b93366a9f14eb831a
skapersk <simon.kaperski@gmail.com>
2024-08-19 23:34:54 +0200

    still working on AMateria

--- cpp04/ex03/sources/MateriaSource.cpp
+++ cpp04/ex03/sources/MateriaSource.cpp
@@ -1,34 +1,63 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MateriaSource.hpp                                  :+:      :+:    :+:   */
+/*   MateriaSource.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 23:54:57 by skapersk          #+#    #+#             */
-/*   Updated: 2024/08/18 23:59:46 by skapersk         ###   ########.fr       */
+/*   Updated: 2024/08/19 23:34:09 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MATERIASOURCE_HPP
-# define MATERIASOURCE_HPP
-
-# include "IMateriaSource.hpp"
-# include "AMateria.hpp"
-
-class MateriaSource : public IMateriaSource {
-	private:
-		AMateria	*saved_materias[4];
-
-	public:
-		MateriaSource();
-		MateriaSource(const MateriaSource &cpy);
-		~MateriaSource();
-
-		MateriaSource &operator=(const MateriaSource &rhs);
-
-		void		learnMateria(AMateria*);
-		AMateria	*createMateria(const std::string &type);
+MateriaSource::MateriaSource() {
+	std::cout << "MateriaSource: Default Constructor called" << std::endl;
+	for (int i = 0; i < 4; i++) {
+		this->saved_materias[i] = NULL;
+	}
 }
 
-#endif
+MateriaSource::MateriaSource(const MateriaSource &cpy) {
+	std::cout << "MateriaSource: Copy constructor called" << std::endl;
+	*this = cpy;
+}
+
+MateriaSource::~MateriaSource() {
+	std::cout << "MateriaSource: Destructor called" << std::endl;
+	for (int i = 0; i < 4; i++)
+		delete this->saved_materias[i];
+}
+
+MateriaSource &MateriaSource::operator=(const MateriaSource &rhs) {
+	std::cout << "MateriaSource: Copy assignment operator called" << std::endl;
+	for (int i = 0; i < 4; i++) {
+		if (this->saved_materias[i] != rhs.saved_materias[i])
+			delete this->saved_materias[i];
+		this->saved_materias[i] = NULL;
+		if (rhs.saved_materias[i]) {
+			this->saved_materias[i] =  rhs.saved_materias[i];
+		}
+	}
+	return (*this);
+}
+
+void		MateriaSource::learnMateria(AMateria *MateriaSource) {
+	for (int i = 0; i < 4; i++) {
+		if (!this->saved_materias[i]) {
+			this->saved_materias[i] = MateriaSource;
+			return ;
+		}
+	}
+}
+
+AMateria	*MateriaSource::createMateria(const std::string &type) {
+	AMateria	*new_materia;
+
+	for (int i = 0; i < 4; i++) {
+		if (this->saved_materias[i] && this->saved_materias[i]->getType() == type) {
+			new_materia = this->saved_materias[i]->clone();
+			return (new_materia);
+		}
+	}
+	return (NULL);
+}
