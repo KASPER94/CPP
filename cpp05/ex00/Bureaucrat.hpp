3e27aef57b114f62d8944097ad559e3d595c5200
skapersk <simon.kaperski@gmail.com>
2024-08-21 17:39:46 +0200

    workin on exception

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 17:21:04 by skapersk          #+#    #+#             */
+/*   Updated: 2024/08/21 17:30:05 by skapersk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <cstring>
+
+class Bureaucrat {
+	private:
+		const std::string _name;
+		int	_grade;
+
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(const Bureaucrat &cpy);
+		~Bureaucrat();
+
+		Bureaucrat &operator=(const Bureaucrat &rhs);
+		std::string getName() const;
+		int getGrade() const;
+}
+
+#endif
