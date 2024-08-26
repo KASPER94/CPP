9b64b10ef40077940f88abe53e5af6c83f750504
Simon Kaperski <skapersk@made-f0Br3s7.clusters.42paris.fr>
2024-08-26 16:46:40 +0200

    ex01 in comin'

--- cpp05/ex01/includes/Form.hpp
+++ cpp05/ex01/includes/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 16:55:22 by skapersk          #+#    #+#             */
-/*   Updated: 2024/08/23 16:58:45 by skapersk         ###   ########.fr       */
+/*   Updated: 2024/08/26 15:40:17 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,22 @@ class Form {
 		const int	_requiredGrade;
 
 	public:
-		Form();
+		Form(std::string _name, );
 		~Form();
-		Form()
+		Form(const Form &cpy);
+		Form &operator=(const Form &rhs);
 
-}
-
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		}
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		}
+		int getGrade() const;
+		bool	beSigned(Bureaucrat &Bureaucrat);
+		void	signForm();
+};
 
 #endif
--- cpp05/ex01/sources/Form.cpp
+++ cpp05/ex01/sources/Form.cpp
@@ -6,8 +6,36 @@
 /*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 16:55:04 by skapersk          #+#    #+#             */
-/*   Updated: 2024/08/23 16:55:14 by skapersk         ###   ########.fr       */
+/*   Updated: 2024/08/26 16:46:21 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
+
+Form::Form(std::string name, bool sign, const int grade, const int reqGrade) _name(name), _signature(sign), _grade(grade), _requiredGrade(reqGrade) {
+
+}
+
+Form::~Form() {
+
+}
+
+Form::Form(const Form &cpy) _name(cpy.name), _signature(cpy.sign), _grade(cpy.grade), _requiredGrade(cpy.reqGrade) {
+	// *this = cpy;
+}
+
+Form &Form::operator=(const Form &rhs) {
+	if (this != &rhs) {
+		this->_name = rhs._name;
+		this->_signature = rhs._signature;
+		this->_grade = rhs._grade;
+		this->_grade = rhs._grade;
+	}
+	return (*this);
+}
+
+bool	Form::beSigned(Bureaucrat &Bureaucrat) {
+	if(Bureaucrat->getGrade() <= this->getGrade()) {
+		this->_
+	}
+}
