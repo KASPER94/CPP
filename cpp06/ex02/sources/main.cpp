b143dd0f5d0117422f23ddecfb189f027d64dc52
skapersk <simon.kaperski@gmail.com>
2024-09-22 19:59:36 +0200

    cpp06 - cast done

---
+++ cpp06/ex02/sources/main.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 12:05:50 by skapersk          #+#    #+#             */
+/*   Updated: 2024/09/22 19:56:07 by skapersk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+#include "Base.hpp"
+#include "AClass.hpp"
+#include "BClass.hpp"
+#include "CClass.hpp"
+
+Base	*generate(void) {
+	int	rand = std::rand() % 3;
+
+	if (rand == 0)
+		return (new AClass);
+	else if (rand == 1)
+		return (new BClass);
+	else
+		return (new CClass);
+}
+
+void	identify(Base *p) {
+	if (dynamic_cast<AClass*>(p))
+		std::cout << "A Class" << std::endl;
+	else if (dynamic_cast<BClass*>(p))
+		std::cout << "B Class" << std::endl;
+	else if (dynamic_cast<CClass*>(p))
+		std::cout << "C Class" << std::endl;
+}
+
+void	identify(Base &p) {
+	Base test;
+
+	try {
+		test = dynamic_cast<AClass&>(p);
+		std::cout << "A Class" << std::endl;
+	}
+	catch (std::exception &e) {}
+	try {
+		test = dynamic_cast<BClass&>(p);
+		std::cout << "B Class" << std::endl;
+	}
+	catch (std::exception &e) {}
+		try {
+		test = dynamic_cast<CClass&>(p);
+		std::cout << "C Class" << std::endl;
+	}
+	catch (std::exception &e) {}
+}
+
+int	main(void) {
+	Base *tmp;
+
+	std::cout << "identify with pointer 10 Classes between A, B and C denerated randomly" << std::endl;
+	for (int i = 0; i < 10; i++) {
+		tmp = generate();
+		identify(tmp);
+		delete tmp;
+	}
+
+	std::cout << "identify with reference 10 Classes between A, B and C denerated randomly" << std::endl;
+	for (int i = 0; i < 10; i++) {
+		tmp = generate();
+		identify(*tmp);
+		delete tmp;
+	}
+}
