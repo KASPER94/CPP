16ab0de7fbe91bb8fd8f954cf9b080bc8646ec13
skapersk <simon.kaperski@gmail.com>
2024-09-24 00:14:48 +0200

    almost found solution for ex01 from cpp07

--- cpp07/ex01/includes/iter.hpp
+++ cpp07/ex01/includes/iter.hpp
@@ -6,23 +6,19 @@
 /*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 11:36:24 by skapersk          #+#    #+#             */
-/*   Updated: 2024/09/23 22:47:51 by skapersk         ###   ########.fr       */
+/*   Updated: 2024/09/24 00:14:07 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
-class Serializer {
-	private:
-		Serializer();
-		~Serializer();
-		Serializer(const Serializer &cpy);
-		Serializer &operator=(const Serializer &rhs);
-		
-	public:
-		static uintptr_t serialize(Data *ptr);
-		static Data	*deserialize(uintptr_t raw);
-};
+# include <iostream>
+
+template<typename T>
+void iter(T *tab, int length, void(*f)(T const &T)) {
+	for (int i = 0; i < length; i++)
+		f(tab[i]);
+}
 
 #endif
--- cpp07/ex01/sources/main.cpp
+++ cpp07/ex01/sources/main.cpp
@@ -6,38 +6,12 @@
 /*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 12:05:50 by skapersk          #+#    #+#             */
-/*   Updated: 2024/09/22 00:37:56 by skapersk         ###   ########.fr       */
+/*   Updated: 2024/09/23 22:49:28 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Serializer.hpp"
+#include "iter.hpp"
 
 int	main(void) {
-	Data 		test;
-	uintptr_t	serialized;
-	Data		*deserialized;
-
-	test.str_test = "new string";
-	std::cout << " str: " << test.str_test << std::endl;
-	test.nb_test = 42;
-	std::cout << " int: " << test.nb_test << std::endl;
-	test.float_test = 42.42f;
-	std::cout << " float: " << test.float_test << "f" << std::endl;
-
-	std::cout << std::endl;
-	serialized = Serializer::serialize(&test);
-	std::cout <<  "Serialized struct Data test is: ";
-	std::cout << serialized << std::endl;
-	std::cout <<  "Serialized struct Data test is: ";
-	std::cout << std::hex << serialized << std::dec << " (in hexadecimal)" << std::endl;
 	
-	std::cout << std::endl;
-	deserialized = Serializer::deserialize(serialized);
-	std::cout <<  "Deserialized struct Data test is: " << std::endl;
-	std::cout << "str: " << deserialized->str_test << std::endl;
-	test.nb_test = 123;
-	std::cout << "nb: " << deserialized->nb_test << std::endl;
-	test.float_test = 123.321f;
-	std::cout << "float: " << deserialized->float_test << std::endl;
-
 }
