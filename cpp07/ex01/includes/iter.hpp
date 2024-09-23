cee26e052f4f2f0b5e4bbf55f24715f036582e2e
skapersk <simon.kaperski@gmail.com>
2024-09-24 00:44:15 +0200

    more test for ex02 from cpp07

--- cpp07/ex01/includes/iter.hpp
+++ cpp07/ex01/includes/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 11:36:24 by skapersk          #+#    #+#             */
-/*   Updated: 2024/09/24 00:19:23 by skapersk         ###   ########.fr       */
+/*   Updated: 2024/09/24 00:25:17 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 
 template<typename T>
 void iter(T *tab, int length, void(*f)(T const &t)) {
+	if (tab == NULL || f == NULL)
+		return ;
 	for (int i = 0; i < length; i++)
 		f(tab[i]);
 }
