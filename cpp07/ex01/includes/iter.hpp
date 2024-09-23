8e3aa4b9adf9e59dc74cf3454d82501c51278a74
skapersk <simon.kaperski@gmail.com>
2024-09-24 00:22:20 +0200

    add test for ex01 from cpp07

--- cpp07/ex01/includes/iter.hpp
+++ cpp07/ex01/includes/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 11:36:24 by skapersk          #+#    #+#             */
-/*   Updated: 2024/09/24 00:14:07 by skapersk         ###   ########.fr       */
+/*   Updated: 2024/09/24 00:19:23 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <iostream>
 
 template<typename T>
-void iter(T *tab, int length, void(*f)(T const &T)) {
+void iter(T *tab, int length, void(*f)(T const &t)) {
 	for (int i = 0; i < length; i++)
 		f(tab[i]);
 }
--- cpp07/ex01/sources/main.cpp
+++ cpp07/ex01/sources/main.cpp
@@ -6,12 +6,29 @@
 /*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 12:05:50 by skapersk          #+#    #+#             */
-/*   Updated: 2024/09/23 22:49:28 by skapersk         ###   ########.fr       */
+/*   Updated: 2024/09/24 00:21:20 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+void display0(int const & i) {
+	std::cout << i << 0<< std::endl;
+}
+
+template<typename T>
+void display0T(T const &t) {
+	std::cout << t << 0 << std::endl;
+}
+
 int	main(void) {
-	
+	int tab[4] = {2, 3, 4, 1};
+	iter(tab, 4, display0);
+	iter(tab, 4, display0T);
+	std::string ts[3] = {
+	"Salut",
+	"Ca",
+	"Va ?"
+	};
+	iter(ts, 3, display0T);
 }
